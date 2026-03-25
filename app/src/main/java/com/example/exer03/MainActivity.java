4231308d24c744c11f5487dac6bc18dd9cf088dd
zraim <zraim.ebrada1@gmail.com>
2026-03-25 20:40:32 +0800

    feat: added the logic for main activity and did the echo xml file

--- app/src/main/java/com/example/exer03/MainActivity.java
+++ app/src/main/java/com/example/exer03/MainActivity.java
@@ -1,18 +1,36 @@
 package com.example.exer03;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityLog";
+    public static final String KEY_USER_MESSAGE = "USER_MESSAGE";
+
+    private EditText edtMessage;
+    private Button btnSendMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+
+        edtMessage = findViewById(R.id.edtMessage);
+        btnSendMessage = findViewById(R.id.btnSendMessage);
+
+        btnSendMessage.setOnClickListener(v -> {
+            String message = edtMessage.getText().toString();
+
+            Intent intent = new Intent(MainActivity.this, EchoActivity.class);
+            intent.putExtra(KEY_USER_MESSAGE, message);
+            startActivity(intent);
+        });
     }
 }
