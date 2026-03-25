b7a85eeba6f76b69a6174d83e2c54ecb097c053f
zraim <zraim.ebrada1@gmail.com>
2026-03-25 19:56:01 +0800

    feat: add input user interface with the EditText and Send Button

---
+++ app/src/main/java/com/example/exer03/EchoActivity.java
@@ -0,0 +1,11 @@
+package com.example.exer03;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EchoActivity extends AppCompatActivity{
+}
