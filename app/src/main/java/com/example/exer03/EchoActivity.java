39d1b54755e2a8632332f03b5ddd61575a54ef85
zraim <zraim.ebrada1@gmail.com>
2026-03-25 21:03:15 +0800

    feat: added the EchoActivity java logic and added string resources

--- app/src/main/java/com/example/exer03/EchoActivity.java
+++ app/src/main/java/com/example/exer03/EchoActivity.java
@@ -7,5 +7,28 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class EchoActivity extends AppCompatActivity{
+public class EchoActivity extends AppCompatActivity {
+
+
+    private TextView txtEcho;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_echo);
+
+
+        txtEcho = findViewById(R.id.txtEcho);
+
+        Intent receivedIntent = getIntent();
+        String message = receivedIntent.getStringExtra(MainActivity.KEY_USER_MESSAGE);
+
+
+
+        if (message != null && !message.isEmpty()) {
+            txtEcho.setText(message);
+        } else {
+            txtEcho.setText(getString(R.string.no_message_received));
+        }
+    }
 }
